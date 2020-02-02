7a450da78b5c1065f7121a70ce7f3a378aeb0c5d
HAN HYUNSEOB <hanhyunseob@gmail.com>
2020-02-02 21:10:10 +0900

    20.2.2

--- js/index.js
+++ js/index.js
@@ -19,6 +19,39 @@ $(document).ready(function () {
       }
   }
 
+/* header */
+
+$('.btn_menu').on('click', function () {
+  if ($(this).hasClass('active')) { //닫기
+    $gnb.stop().animate({left: '100%'}, 300, function () {
+      $(this).css({visibility: 'hidden'}).find('ul li.on').removeClass('on').children('ul').stop().slideUp();
+    });
+
+    $(this).removeClass('active').find('blind').text('메뉴 열기');
+  } else {    //열기
+    var scrollMove = scrollT;  //click시 스크롤을 저장
+
+    $(this).addClass('active').find('.sr-only').text('메뉴 닫기');
+
+    var $first = $gnb.find('[data-link="first"]');
+    var $last = $gnb.find('[data-link="last"]');
+
+    $gnb.css({visibility: 'visible'}).stop().animate({left: 0}, 300, function () {
+      $first.focus();   //대상 엘리먼트에 포커스를 강제로 이동
+    });
+
+    //첫번째 a 태그에서 shift+tab을 눌러 header의 영역으로 포커스가 이동하지 못하게 제한
+    $first.on('keydown', function (e) {
+      console.log(e.keyCode);   //tab을 클릭하면 9를 반환
+      if ( e.shiftKey && e.keyCode == 9) {
+        e.preventDefault();   //포커스 이동을 강제로 제한
+        $last.focus();        //처음 포커스로 이동, #gnbWrap을 벗어나지 않고 순환됨
+      }
+    });
+
+  }
+});
+
   /* cnt3 */
   var mainCtr = $("#main-ctr"),
   hello = $(".hello"),
@@ -183,6 +216,18 @@ $('#modal2 a').eq(1).on('click', function (e) {
 $('#modal2 a').eq(2).on('click', function (e) {
   e.preventDefault(); //a 기본 기능 제한
   $('#modal2 img').eq(1).css('display', 'block').siblings('img, video').css('display', 'none');
+});
+$('#modal3 a').eq(0).on('click', function (e) {
+  e.preventDefault(); //a 기본 기능 제한
+  $('#modal3 video').eq(0).css('display', 'block').siblings('img').css('display', 'none');
+});
+$('#modal3 a').eq(1).on('click', function (e) {
+  e.preventDefault(); //a 기본 기능 제한
+  $('#modal3 img').eq(0).css('display', 'block').siblings('img, video').css('display', 'none');
+});
+$('#modal3 a').eq(2).on('click', function (e) {
+  e.preventDefault(); //a 기본 기능 제한
+  $('#modal3 img').eq(1).css('display', 'block').siblings('img, video').css('display', 'none');
 });
 
 });
